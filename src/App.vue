46abbc45ebf1f0819480100f77e5036d5cc09b31
giulianobre <giuliarnobre@gmail.com>
2023-02-24 14:26:01 -0300

    add

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,24 @@
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 
-export default {
-  data() {
-    return {
-      contador: 5
-    }
-  },
-  methods: {
-    incrementarContador() {
-      this.contador++
-    },
-    decrementarContador() {
-      this.contador--
-    }
-  },
-  mounted() {
-    console.log(`O valor inicial do contador é ${this.contador}.`)
-  }
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const contador = ref(0)
+
+function incrementarContador() {
+  contador.value++
+}
+function decrementarContador() {
+  contador.value--
 }
 
-
-
-
-
+onMounted(() => {
+  console.log(`O valor inicial do contador é ${contador.value}.`)
+})
 </script>
 
 <template>
-  <button @click="incrementarContador">Incrementar</button>
-  <button @click="decrementarContador">Decrementar</button>
+  <button @click="incrementarContador()">Incrementar</button>
+  <button @click="decrementarContador()">Decrementar</button>
   <p>Valor do contador é: {{ contador }}</p>
 </template>
 
