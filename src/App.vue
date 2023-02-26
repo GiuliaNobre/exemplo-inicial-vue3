c283925f33a54a82dcec4f57637d72c1c75c9a94
GiuliaNobre <106554467+GiuliaNobre@users.noreply.github.com>
2023-02-26 13:43:58 -0300

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,43 @@
-
 <script setup>
-import { ref, onMounted } from 'vue'
+    const nome = 'João'
+    const idade = 25
 
-const contador = ref(0)
+    function inverter(texto) {
+        return texto.split('').reverse().join('')
+    }
 
-function incrementarContador() {
-  contador.value++
-}
-function decrementarContador() {
-  contador.value--
-}
-
-onMounted(() => {
-  console.log(`O valor inicial do contador é ${contador.value}.`)
-})
+    function saudacao() {
+        return `Olá, ${nome}!`
+    }
 </script>
 
 <template>
-  <button @click="incrementarContador()">Incrementar</button>
-  <button @click="decrementarContador()">Decrementar</button>
-  <p>Valor do contador é: {{ contador }}</p>
+    <div class="info">
+        <h1>Exemplo 1 - Mostrando informações na tela</h1>
+        <p>Nome: {{ nome }} </p>
+        <p>Idade: {{ idade }} anos</p>
+        <hr />
+        <p>Para completar 50 anos faltam: {{ (50-idade) }} anos</p>
+        <p>O nome tem {{ nome.length }} caracteres</p>
+        <p>O nome invertido é: </p>
+        <hr />
+        <p> Exemplo de saudação usando função</p>
+        
+      
+    </div>
 </template>
 
 <style scoped>
-button {
+.info {
+  background-color: rgba(35,12,22,1);
+  padding: 20px 30px;
+  color: rgb(183, 210, 219);
+  border-radius: 10px;
+}
+
+h1 {
+  color: white;
   font-weight: bold;
-  background-color: #a34caf;
-  color: rgb(248, 222, 222);
-  border: 2px solid #070404;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  
-}
-</style>
-
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  font-size: 1.5em;
 }
 </style>
